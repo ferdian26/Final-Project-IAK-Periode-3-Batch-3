7dbf931dac0cb4187e376bb3f5ea46ee1a8441fa
Dhanes <nes07197@gmail.com>
2017-12-09 23:23:40 +0800

    final project Janani Dhaneswara

---
+++ Janani Dhaneswara/app/src/main/java/com/example/dhanes/drivemode/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.dhanes.drivemode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView maps;
+    private ImageView people;
+    private ImageView call;
+    private ImageView voice;
+    private ImageView music;
+    private ImageView home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        maps = findViewById(R.id.maps);
+        people = findViewById(R.id.people);
+        call = findViewById(R.id.call);
+        voice = findViewById(R.id.voice);
+        music = findViewById(R.id.music);
+        home = findViewById(R.id.home);
+
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "go to maps app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        people.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "go to people app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "go to call app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        voice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "go to voice app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "go to music app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ Janani Dhaneswara/app/src/main/java/com/example/dhanes/drivemode/Opening.java
@@ -0,0 +1,27 @@
+package com.example.dhanes.drivemode;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Opening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+
+        setTimer(2000, MainActivity.class);
+    }
+
+        private void setTimer (int delay, final Class intent){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(getApplicationContext(), intent));
+                    finish();
+                }
+        },delay);
+    }
+}
